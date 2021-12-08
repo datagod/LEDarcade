eb266173d639dc818581832dbdbd76d7cc733bf5
datagod <william.mcevoy@gmail.com>
2021-12-08 16:54:54 -0500

    Created new repositry

---
+++ CustomMessages.py
@@ -0,0 +1,38 @@
+ConnectionMessages = [
+  'connection accepted',
+  'Hello?  Who is this!?',
+  'Your call cannot be ... oh wait.. nevermind',
+  'busy signal, try again.  Just kidding!',
+  'Who?  Just a second...',
+  'low bandwidth detected...',
+  'boom baby!',
+  'is it safe?',
+  'Sorry, nobody home',
+  'oh.  you again.  Well thats just great...',
+  'access granted, for now',
+  'connected.  Try not to wreck the place',
+  'connected.  Fun times for all!',
+  'intergalactic static detected... trying again',
+  'Are you sure you want to continue?',
+  'All hail our new LED clock overlord',
+  'insert 25 cents to continue...',
+  "today's your lucky day",
+  "do you feel lucky punk?",
+  "enter at your own risk"
+
+]
+
+ChatStartMessages = [
+  '/me has entered the chat',
+  '/me just showed up',   
+  '/me is here for the lulz',
+  'Howdy Folks!',
+  '/me is here!',
+  '/me wanders around aimlessly',
+  '/me has come in from the cold',
+  '/me walks in, looks around, makes himself comfortable',
+  'Hey everyone!',
+  'Glad to be here.',
+  'Greetings programs!'
+  
+]
