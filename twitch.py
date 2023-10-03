81e99834fd96433db89b0dae2bb62ce29e346989
datagod <william.mcevoy@gmail.com>
2023-10-03 13:17:40 -0400

    fix

--- twitch.py
+++ twitch.py
@@ -1951,8 +1951,6 @@ class Bot(commands.Bot ):
     @commands.command()
     async def taco(self, ctx: commands.Context):
       if(SHOW_CHATBOT_MESSAGES == True):
-        message = "Enjoy the peaceful starry sky while staring at a clock"
-      await self.Channel.send(message)
       print("CHAT| TACO detected")
       LED.StarryNightDisplayText(
       Text1       = "TACO",
