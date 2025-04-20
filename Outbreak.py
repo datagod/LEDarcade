96ae61cc79b515d41bf6029cdf8f0383be9f4bf9
datagod <william.mcevoy@gmail.com>
2025-04-19 22:37:21 -0400

    fix

--- Outbreak.py
+++ Outbreak.py
@@ -2713,6 +2713,7 @@ def CreateDinnerPlate(MapLevel):
         "O            |                                                   O",
         "O            |@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@O",
         "O            |                                                   O",
+        "O            |                                                   O",
         "O            |***************************************************O",
         "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
     )
