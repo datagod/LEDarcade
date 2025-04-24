b6aa9dbbd407021ca46d97c8ef2f8802e50b9714
datagod <william.mcevoy@gmail.com>
2025-04-24 13:08:06 -0400

    stock ticker changes

---
+++ StockTicker.py
@@ -0,0 +1,124 @@
+# %%
+
+'''
+TO DO:
+ 
+'''
+
+
+
+import os
+os.system('cls||clear')
+
+import sys
+import re   # regular expression
+
+import LEDarcade as LED
+from rgbmatrix import graphics
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+import random
+from configparser import ConfigParser
+import requests
+import traceback
+import socket
+
+
+
+import json
+
+
+
+
+import pprint
+
+
+import time
+from datetime import datetime, timezone
+
+
+
+#---------------------------------------
+#Variable declaration section
+#---------------------------------------
+ScrollSleep         = 0.025
+TerminalTypeSpeed   = 0.02  #pause in seconds between characters
+TerminalScrollSpeed = 0.02  #pause in seconds between new lines
+CursorRGB           = (0,255,0)
+CursorDarkRGB       = (0,50,0)
+
+
+
+
+HatHeight = 32
+HatWidth  = 64
+StreamBrightness = 20
+GifBrightness    = 25
+MaxBrightness    = 80
+
+  
+
+
+#Sprite display locations
+LED.ClockH,      LED.ClockV,      LED.ClockRGB      = 0,0,  (0,150,0)
+LED.DayOfWeekH,  LED.DayOfWeekV,  LED.DayOfWeekRGB  = 8,20,  (125,20,20)
+LED.MonthH,      LED.MonthV,      LED.MonthRGB      = 28,20, (125,30,0)
+LED.DayOfMonthH, LED.DayOfMonthV, LED.DayOfMonthRGB = 47,20, (115,40,10)
+
+#Colors
+TerminalRGB = (0,200,0)
+CursorRGB = (0,75,0)
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+# MAIN SECTION                                                               --
+#------------------------------------------------------------------------------
+
+
+
+print ("---------------------------------------------------------------")
+print ("WELCOME TO THE LED ARCADE - Stock Price Displayorama            ")
+print ("")
+print ("BY DATAGOD and The Blue Friend")
+print ("")
+print ("This program will display the stock prices for various ")
+print ("stock symbols.")
+print ("---------------------------------------------------------------")
+print ("")
+print ("")
+
+
+
+
+
+
+
+print ("--Start--")
+#Fake boot sequence
+LED.ClearBigLED()
+LED.ClearBuffers()
+CursorH = 0
+CursorV = 0
+LED.ScreenArray,CursorH,CursorV = LED.TerminalScroll(LED.ScreenArray,"LED Stock Tracker",CursorH=CursorH,CursorV=CursorV,MessageRGB=(100,100,0),CursorRGB=(0,255,0),CursorDarkRGB=(0,50,0),StartingLineFeed=1,TypeSpeed=TerminalTypeSpeed,ScrollSpeed=TerminalTypeSpeed)
+LED.ScreenArray,CursorH,CursorV = LED.TerminalScroll(LED.ScreenArray,"by datagod and The Blue Friend",CursorH=CursorH,CursorV=CursorV,MessageRGB=(100,100,0),CursorRGB=(0,255,0),CursorDarkRGB=(0,50,0),StartingLineFeed=1,TypeSpeed=TerminalTypeSpeed,ScrollSpeed=TerminalTypeSpeed)
+LED.ScreenArray,CursorH,CursorV = LED.TerminalScroll(LED.ScreenArray,".........................",CursorH=CursorH,CursorV=CursorV,MessageRGB=(100,100,0),CursorRGB=(0,255,0),CursorDarkRGB=(0,50,0),StartingLineFeed=1,TypeSpeed=0.025,ScrollSpeed=ScrollSleep)
+LED.ScreenArray,CursorH,CursorV = LED.TerminalScroll(LED.ScreenArray,"Boot sequence initiated",CursorH=CursorH,CursorV=CursorV,MessageRGB=(100,100,0),CursorRGB=(0,255,0),CursorDarkRGB=(0,50,0),StartingLineFeed=1,TypeSpeed=0.005,ScrollSpeed=ScrollSleep)
+LED.ScreenArray,CursorH,CursorV = LED.TerminalScroll(LED.ScreenArray,"NASDAQ CONNECTION",CursorH=CursorH,CursorV=CursorV,MessageRGB=(100,100,0),CursorRGB=(0,255,0),CursorDarkRGB=(0,50,0),StartingLineFeed=1,TypeSpeed=0.005,ScrollSpeed=ScrollSleep)
+LED.ScreenArray,CursorH,CursorV = LED.TerminalScroll(LED.ScreenArray,"OK",CursorH=CursorH,CursorV=CursorV,MessageRGB=(100,100,0),CursorRGB=(0,255,0),CursorDarkRGB=(0,50,0),StartingLineFeed=1,TypeSpeed=0.005,ScrollSpeed=ScrollSleep)
+LED.ScreenArray,CursorH,CursorV = LED.TerminalScroll(LED.ScreenArray,"CURRENCY LEVELS",CursorH=CursorH,CursorV=CursorV,MessageRGB=(100,100,0),CursorRGB=(0,255,0),CursorDarkRGB=(0,50,0),StartingLineFeed=1,TypeSpeed=0.005,ScrollSpeed=ScrollSleep)
+LED.ScreenArray,CursorH,CursorV = LED.TerminalScroll(LED.ScreenArray,"OK",CursorH=CursorH,CursorV=CursorV,MessageRGB=(100,100,0),CursorRGB=(0,255,0),CursorDarkRGB=(0,50,0),StartingLineFeed=1,TypeSpeed=0.005,ScrollSpeed=ScrollSleep)
+LED.BlinkCursor(CursorH= CursorH,CursorV=CursorV,CursorRGB=CursorRGB,CursorDarkRGB=CursorDarkRGB,BlinkSpeed=0.5,BlinkCount=2)
+IPAddress = LED.ShowIPAddress(Wait=5)
+
+
+
+
+
+# %%
+
+ 
