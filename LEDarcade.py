7482371d62bb8134f26dedff57075ed3b074e64f
datagod <william.mcevoy@gmail.com>
2025-04-28 22:26:06 -0400

    test

--- LEDarcade.py
+++ LEDarcade.py
@@ -19449,18 +19449,20 @@ def DisplayStockPrice(StockPrice=""):
     v = 10
 
     #ClearBigLED()
-    #ClearBuffers()
+    ClearBuffers()
     global ScreenArray
 
     StockSprite = CreateBannerSprite(StockPrice)
     
     #MakeAndShowClock(hh,h,v,RGB,ShadowGreen,ZoomFactor,Fill=False)
-    ScreenArray1  = ([[]])
-    ScreenArray1  = [[ (0,0,0) for i in range(HatWidth)] for i in range(HatHeight)]
-    ScreenArray2  = ([[]])
-    ScreenArray2  = [[ (0,0,0) for i in range(HatWidth)] for i in range(HatHeight)]
-  
+    
+    # Initialize blank screen arrays
+    ScreenArray1 = [[(0, 0, 0) for _ in range(HatWidth)] for _ in range(HatHeight)]
+    ScreenArray2 = [[(0, 0, 0) for _ in range(HatWidth)] for _ in range(HatHeight)]
+
+    
     ScreenArray1 = CopySpriteToScreenArrayZoom(StockSprite,h-1,v+1,ShadowRGB,(0,0,0),ZoomFactor=ZoomFactor,Fill=False,InputScreenArray=ScreenArray)
     ScreenArray1 = CopySpriteToScreenArrayZoom(StockSprite,h,v,RGB,(0,0,0),ZoomFactor=ZoomFactor,Fill=False,InputScreenArray=ScreenArray1)
+    
     TransitionBetweenScreenArrays(ScreenArray2,ScreenArray1,TransitionType=2)
   
